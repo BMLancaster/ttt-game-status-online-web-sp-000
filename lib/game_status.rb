62f33478a62488aff57bca7619ca8dd88100bf57
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-10-25 13:17:25 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,41 +4,51 @@ def position_taken?(board, index)
 end
 
 WIN_COMBINATIONS = [
-  [0,1,2], #Top row
-  [3,4,5], #Middle row
-  [6,7,8], #Bottom row
-  [0,3,6], #Left column
-  [1,4,7], #Middle column
-  [2,5,8], #Right column
-  [0,4,8], #Left start diagonal
-  [2,4,6]  #Right start diagonal
+  [0,1,2,["Top Row"]], #Top row
+  [3,4,5,["Middle Row"]], #Middle row
+  [6,7,8,["Bottow Row"]], #Bottom row
+  [0,3,6,["Left Column"]], #Left column
+  [1,4,7,["Middle Column"]], #Middle column
+  [2,5,8,["Right Column"]], #Right column
+  [0,4,8,["Left Start Diagonal"]], #Left start diagonal
+  [2,4,6,["Right Start Diagonal"]]  #Right start diagonal
 ]
 
 def won?(board)
-WIN_COMBINATIONS.each do |win_set|
-  if win_set = WIN_COMBINATIONS[0]
-    puts "Top row win!"
-  elsif win_set = WIN_COMBINATIONS[1]
-    puts "Middle row win!"
-    elsif win_set = WIN_COMBINATIONS[2]
-    puts "Bottom row win!"
-    elsif win_set = WIN_COMBINATIONS[3]
-    puts "Left column win!"
-    elsif win_set = WIN_COMBINATIONS[4]
-    puts "Middle column win!"
-    elsif win_set = WIN_COMBINATIONS[5]
-    puts "Right column win!"
-    elsif win_set = WIN_COMBINATIONS[6]
-    puts "Left start diagonal win!"
-    elsif win_set = WIN_COMBINATIONS[7]
-    puts "Right start diagonal win!"
-    return true
-   else
-     puts "No victor has emerged."
-     return false
-  end
+WIN_COMBINATIONS.each do |win_set| #1st array iterator
+  win_set.each do |win_type|      #2nd array iterator
+  
+if board == win_set
+  puts "#{win_type} win!"
+  return true
+else
+  return false
 end
 end
+  
+ #if win_set = WIN_COMBINATIONS[0]
+   # puts "Top row win!"
+  #elsif win_set = WIN_COMBINATIONS[1]
+    #puts "Middle row win!"
+    #elsif win_set = WIN_COMBINATIONS[2]
+    #puts "Bottom row win!"
+    #elsif win_set = WIN_COMBINATIONS[3]
+    #puts "Left column win!"
+    #elsif win_set = WIN_COMBINATIONS[4]
+    #puts "Middle column win!"
+    #elsif win_set = WIN_COMBINATIONS[5]
+    #puts "Right column win!"
+    #elsif win_set = WIN_COMBINATIONS[6]
+    #puts "Left start diagonal win!"
+    #elsif win_set = WIN_COMBINATIONS[7]
+   #puts "Right start diagonal win!"
+  #return true
+   #else
+   #puts "No victor has emerged."
+  #return false
+ #end
+#end
+#end
 
 def full?(array)
  if !(array.any?{|i| i == " "})
